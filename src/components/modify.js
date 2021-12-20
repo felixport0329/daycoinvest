23040417949eb3c25661557eef55481680739cbc
Junghoon Lee <junghoon.lee@arm.com>
2021-12-20 00:41:10 +0000

    - data picker. set data as initial - from timestamp (firebase) to data (js)

--- src/components/modify.js
+++ src/components/modify.js
@@ -13,93 +13,61 @@ import TimePicker from '@mui/lab/TimePicker';
 
 function Modify({changePage}) {
 
-    console.log("modify");
     useEffect( async() => {
         const querySnapshot = await getDocs(collection(db, "invest"));
-        console.log("useeffect");
         let c = querySnapshot.docs.map((doc)=>({...doc.data(), id: doc.id}))
-        //console.log(c);
-        setInvestState(c);
-        //setInvestState(querySnapshot.docs.map((doc)=>({...doc.data(), id: doc.id})));
-        //--> state변경에 따른 re-rendering 예상. -> modify 전체를 re-rendering하게 된다.
-        //useEffect로 db에서 값 가져 오면, 다시 한번 그려지게 된다.
+        //[
+        //     {state:"ACTIVE", cointype:"BitCoin", balance:"3000", time:"13:00", id:},
+        //     {state:"INACTIVE", cointype:"Ether", balance:"2000", time:"14:00", id:},
+        //]
 
-        // const [investState, setInvestState] = useState([
-        //     {state:"ACTIVE", cointype:"BitCoin", balance:"3000", time:"13:00"},
-        //     {state:"INACTIVE", cointype:"Ether", balance:"2000", time:"14:00"},
-        // ])
-
-        // querySnapshot.forEach((doc) => {
-        //     // doc.data() is never undefined for query doc snapshots
-        //      console.log(doc.id, " => ", doc.data());
-        //      //setInvestState(querySnapshot.docs.map((doc)=>({...doc.data(), id: doc.id })))
-        //      //setInvestState(querySnapshot.docs.map((doc)=>({...doc.data(), id: doc.id })));
-        // });
+        // convert from timestamp (firebase) to date (js) object
+        // https://firebase.google.com/docs/reference/js/v8/firebase.firestore.Timestamp#todate
+        // c.forEach(function myF(value, index, array){
+        c.forEach((value, index, array) => {
+            value.time = value.time.toDate();
+        });
+        //NOTE: expect re-renderting based on value of state changed
+        //When getting date from firebase throuhg useEffect, it must be re-rendering
+        setState(c);
     },[]);
 
+    
+    //Date to Timestamp in order to insert it into firebase
+    //1. https://www.youtube.com/watch?v=GM32D6xLhZY&ab_channel=kagayajohn 
+    //2. https://www.youtube.com/watch?v=ASF7zvlDCFk&ab_channel=DedicatedManagers
 
+    const [state, setState] = useState([])
 
-    // const modify = () => {
-        
-    // }
-    //https://www.youtube.com/watch?v=tojwQEdI-QI&ab_channel=Codevolution
-    //https://www.youtube.com/watch?v=GM32D6xLhZY&ab_channel=kagayajohn 집어 넣기
-    //https://www.youtube.com/watch?v=ASF7zvlDCFk&ab_channel=DedicatedManagers  집어 넣기
-
-    const [investState, setInvestState] = useState([])
-    const [balance, setBalance] = useState([{balance:0},{balance:2}])
-    const [state, setState] = useState({
-        // balance: Array(2).fill(2),
-        balance: [1,2],
-    });
-        
-
-    const dateConvert = (t) => { 
-        console.log(t);
-        console.log(t.constructor.name);
-        const cc = t.toDate();   // firebase method  //https://firebase.google.com/docs/reference/js/v8/firebase.firestore.Timestamp#todate
-        return cc;
-    }
-
-    const [value, setValue] = React.useState(null);
-    console.log("before map");
-    const renderInvestStateDisp = investState.map((item, index)=> {
-
-        // setValue(item.time);
-        //value = item.time;
-        console.log("map");
+    const renderInvestStateDisp = state.map((item, index)=> {
         return (  //JSX
             <div key={item.id}>
                 <span>{item.state} </span> 
                 <span>{item.cointype} </span>
-                <input name="balance" 
-                    //value={state.balance[index]} 
+                <input
+                    // we don't need name as we're not going to update a specific input component. (I don't know how it can)
                     value={item.balance}
                     onChange={(e)=>{ 
-                        // let a = state.balance.slice();
-                        // a[index] = e.target.value;
-                        // setState( {...state, [e.target.name]: a} );  // ...연산자
-                        investState[index].balance = e.target.value; //이게 되네? const인데?
-                        setInvestState([...investState]);
+                        state[index].balance = e.target.value; //Could it be possible? even though it's const
+                        setState([...state]);
                         }}>
                 </input>
-                {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
+                {/* https://www.youtube.com/watch?v=tojwQEdI-QI&ab_channel=Codevolution    Data Picker */}
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <TimePicker
                     label="Basic example"
-                    value={value}
+                    value={item.time}
                     onChange={(newValue) => {
-                    setValue(newValue);
-                    console.log(newValue);
+                    state[index].time = newValue;
+                    setState([...state]);
                     }}
                     renderInput={(params) => <TextField {...params} />}
                 />
-                </LocalizationProvider> */}
+                </LocalizationProvider>
             </div>
         );
     });
 
-    console.log("before return main");
-    // modify function return
     return (
     <div>
         {renderInvestStateDisp}
@@ -108,7 +76,4 @@ function Modify({changePage}) {
     );
 }
 
-//Can't resolve '@emotion/react'
-// npm install @emotion/react
-// / npm install @emotion/core
 export default Modify;
